eb8661be6ad82a7e44e63239b01a325b6c8f6d60
EndlessJY <33413681+EndlessJY@users.noreply.github.com>
2025-03-20 23:20:02 +0800

    Update build.sh

--- raspberrypi/24.10/build.sh
+++ raspberrypi/24.10/build.sh
@@ -26,7 +26,6 @@ PACKAGES="$PACKAGES luci-i18n-passwall-zh-cn"
 PACKAGES="$PACKAGES luci-app-openclash"
 PACKAGES="$PACKAGES luci-i18n-homeproxy-zh-cn"
 PACKAGES="$PACKAGES openssh-sftp-server"
-PACKAGES="$PACKAGES luci-app-openclash"
 
 # 增加几个必备组件 方便用户安装iStore
 PACKAGES="$PACKAGES fdisk"
